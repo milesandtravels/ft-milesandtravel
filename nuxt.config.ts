9ad06c54b9d0bc3ec58a737f09f32e6e55aa6170
George Tassiano Melo Pereira <george@milesandtravels.com>
2025-06-16 08:14:21 -0300

    ssr for client only

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  ssr: false,
   devtools: { enabled: true },
   modules: [
     'vuetify-nuxt-module',
