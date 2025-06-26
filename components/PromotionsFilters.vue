a345e0bc3709f91f9dcd4f4816f52e15429b7d5a
Aparicio Lemes Neto <aparicio.neto@ext.leroymerlin.pt>
2025-06-25 22:27:48 -0300

    feat: melhorar filtros de promoções

--- components/PromotionsFilters.vue
+++ components/PromotionsFilters.vue
@@ -2,45 +2,31 @@
   <v-dialog
     :model-value="modelValue"
     @update:model-value="$emit('update:modelValue', $event)"
-    fullscreen
-    transition="dialog-bottom-transition"
+    :fullscreen="$vuetify.display.mobile"
+    :max-width="$vuetify.display.mobile ? undefined : '700px'"
+    :transition="$vuetify.display.mobile ? 'dialog-bottom-transition' : 'dialog-transition'"
+    scrollable
   >
     <v-card>
       <v-toolbar color="primary" dark>
         <v-btn icon @click="$emit('update:modelValue', false)">
           <v-icon>mdi-close</v-icon>
         </v-btn>
-        <v-toolbar-title>Filtros</v-toolbar-title>
+        <v-toolbar-title>Filtrar Promoções</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn text @click="$emit('clear')">Limpar</v-btn>
-        <v-btn text @click="$emit('apply')">Aplicar</v-btn>
       </v-toolbar>
 
       <v-card-text class="pa-6">
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6">
+        <v-container class="pa-0">
+          <!-- Campos de alta prioridade - sempre visíveis -->
+          <v-row dense class="mb-3">
+            <v-col cols="12">
               <v-autocomplete
                 :model-value="filters.categories"
                 @update:model-value="updateFilter('categories', $event)"
                 :items="filterOptions.categories"
-                label="Categoria de ecommerce"
+                label="Categoria"
                 variant="outlined"
-                multiple
-                chips
-                clearable
-                closable-chips
-              ></v-autocomplete>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-autocomplete
-                :model-value="filters.promotionTypes"
-                @update:model-value="updateFilter('promotionTypes', $event)"
-                :items="filterOptions.promotionTypes.options"
-                label="Tipo de promoção"
-                variant="outlined"
-                item-title="label"
-                item-value="value"
                 multiple
                 chips
                 clearable
@@ -49,7 +35,7 @@
             </v-col>
           </v-row>
 
-          <v-row>
+          <v-row dense class="mb-3">
             <v-col cols="12" md="6">
               <v-autocomplete
                 :model-value="filters.ecommerces"
@@ -76,6 +62,25 @@
 
             <v-col cols="12" md="6">
               <v-autocomplete
+                :model-value="filters.promotionTypes"
+                @update:model-value="updateFilter('promotionTypes', $event)"
+                :items="filterOptions.promotionTypes.options"
+                label="Tipo de Promoção"
+                variant="outlined"
+                item-title="label"
+                item-value="value"
+                multiple
+                chips
+                clearable
+                closable-chips
+              ></v-autocomplete>
+            </v-col>
+          </v-row>
+
+          <!-- Programas de benefícios - prioridade média -->
+          <v-row dense class="mb-3">
+            <v-col cols="12" md="6">
+              <v-autocomplete
                 :model-value="filters.pointsPrograms"
                 @update:model-value="updateFilter('pointsPrograms', $event)"
                 :items="filterOptions.pointsPrograms"
@@ -97,9 +102,7 @@
                 </template>
               </v-autocomplete>
             </v-col>
-          </v-row>
 
-          <v-row>
             <v-col cols="12" md="6">
               <v-autocomplete
                 :model-value="filters.milesPrograms"
@@ -123,7 +126,9 @@
                 </template>
               </v-autocomplete>
             </v-col>
+          </v-row>
 
+          <v-row dense class="mb-3">
             <v-col cols="12" md="6">
               <v-autocomplete
                 :model-value="filters.cashbackPrograms"
@@ -149,13 +154,16 @@
             </v-col>
           </v-row>
 
-          <v-row>
+          <!-- Ordenação - menor prioridade -->
+          <v-divider class="my-3"></v-divider>
+          
+          <v-row dense>
             <v-col cols="12" md="6">
               <v-select
                 :model-value="filters.orderBy"
                 @update:model-value="updateFilter('orderBy', $event)"
                 :items="filterOptions.orderBy.options"
-                label="Ordenar por"
+                label="Ordenar Por"
                 variant="outlined"
                 item-title="label"
                 item-value="value"
@@ -168,7 +176,7 @@
                 :model-value="filters.order"
                 @update:model-value="updateFilter('order', $event)"
                 :items="filterOptions.order.options"
-                label="Ordem"
+                label="Direção da Ordenação"
                 variant="outlined"
                 item-title="label"
                 item-value="value"
@@ -178,6 +186,25 @@
           </v-row>
         </v-container>
       </v-card-text>
+
+      <!-- Botões de ação fixos na parte inferior -->
+      <v-card-actions class="px-6 py-3">
+        <v-spacer></v-spacer>
+        <v-btn 
+          variant="text" 
+          @click="$emit('clear')"
+          class="mr-3"
+        >
+          Limpar
+        </v-btn>
+        <v-btn 
+          color="primary"
+          variant="flat"
+          @click="$emit('apply')"
+        >
+          Aplicar
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
