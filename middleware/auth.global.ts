c432df27976049d20b527c03256318a13a7624bf
Aparicio Lemes Neto <aparicio.neto@ext.leroymerlin.pt>
2025-07-03 19:59:28 -0300

    fix: ajustar middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,20 +1,17 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { value: user }: any = useSanctumUser()
 
-
   // Manda para home se já tiver com e-mail verificado
-   if (to.path === '/confirmation-email' && user?.email_verified_at) {
+  if (to.path === '/confirmation-email' && user?.email_verified_at) {
     return navigateTo('/')
   }
 
-
   // Excluir a rota de confirmação da verificação
-  if (to.path === '/confirmation-email' || 'callback-email-confirmation') {
+  if (to.path === '/confirmation-email' || to.path === '/callback-email-confirmation') {
     return
   }
 
   if (user && !user?.email_verified_at) {
-    console.log('⚠️ Email not verified - redirecting to confirmation')
     return navigateTo(
       '/confirmation-email?email=' + encodeURIComponent(user.email)
     )
