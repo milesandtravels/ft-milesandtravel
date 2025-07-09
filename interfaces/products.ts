41668734b375654b9d1a551689121b89a7b0a836
Aparicio Lemes Neto <aparicio.neto@ext.leroymerlin.pt>
2025-07-09 13:44:13 -0300

    feat: atualizar interface de produtos

--- interfaces/products.ts
+++ interfaces/products.ts
@@ -1,21 +1,21 @@
 export interface Ecommerce {
-  id: number;
-  name: string;
-  logo_url: string;
-  category: string;
+  id: number
+  name: string
+  logo_url: string
+  category: string
 }
 
 export interface Product {
-  id: number;
-  name: string;
-  price: number;
-  image_url: string;
-  product_url: string;
-  created_at: string; // ISO 8601 date string
-  updated_at: string; // ISO 8601 date string
-  reviews: number;
-  rating: string; // pode ser convertido para número se necessário
-  ecommerce: Ecommerce;
-  isCustomProduct: boolean;
-  selected: boolean;
+  id: number
+  name: string
+  price: number
+  image_url: string
+  product_url: string
+  created_at: string // ISO 8601 date string
+  updated_at: string // ISO 8601 date string
+  reviews: number
+  rating: string // pode ser convertido para número se necessário
+  ecommerce: Ecommerce
+  isCustomProduct: boolean
+  selected: boolean
 }
